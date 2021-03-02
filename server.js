7a1437cd3c526a524abb5ba4ad3054ab1b27b26e
Linh Phan <linh.phan@novobi.vn>
2021-03-02 17:57:00 +0700

    add login page

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const app = express();
 app.set('views', 'public/views');
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.render('login');
 });
+
+app.post('/login', (req, res) => {
+    res.send('It \'s Ok');
+})
 
 app.listen(8080, () => {
     console.log('Server listen on port 8080');
