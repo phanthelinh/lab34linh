ff56fe498f4b8ffe3b84ce2f8b063d0135d66b63
Linh Phan <linh.phan@novobi.vn>
2021-03-04 01:22:10 +0700

    update lab 3

--- server.js
+++ server.js
@@ -1,21 +1,61 @@
+const { static } = require('express');
 const express = require('express');
-
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
-app.set('views', 'public/views');
+app.use(session({
+    secret: process.env.SESSION_SECRET || '', 
+    saveUninitialized: false,
+    resave: true,
+    cookie: {
+        maxAge: 1800000
+    }
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+const redirectLogin = (req, res, next) => {
+    if (!req.session.useremail) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
 
-app.get('/', (req, res) => {
-    res.render('login');
+const redirectHome = (req, res, next) => {
+    if (req.session.useremail) {
+        res.redirect('/home');
+    } else {
+        next();
+    }
+}
+
+app.get('/', redirectLogin, (req, res) => {
+    res.render('pages/home', {products: JSON.parse(process.env.PRODUCTS_LIST) || []});
 });
 
-app.post('/login', (req, res) => {
-    res.send('It \'s Ok');
+app.get('/login', redirectHome, (req, res) => {
+    res.render('pages/login');
 })
 
+app.post('/login', redirectHome, (req,res) => {
+    // get the request body
+    var loginUser = req.body || {email: '', password: ''};
+    if (loginUser.email === process.env.LOGIN_EMAIL && loginUser.password === process.env.LOGIN_PASSWORD) {
+        // redirect
+        req.session.useremail = loginUser.email;
+        res.redirect('/');
+    } else {
+        res.render('pages/login', {message: 'Invalid email or password'});
+    }
+});
+
+
 app.listen(8080, () => {
-    console.log('Server listen on port 8080');
+    console.log('app listen on port 8080');
 })
